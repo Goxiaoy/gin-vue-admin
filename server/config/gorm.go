d8da286f0dad74595a29d02dfbf2d7f70dad1123
Goxiaoy <xy1996mail@gmail.com>
2020-11-27 21:41:19 +0800

    use dsn

--- server/config/gorm.go
+++ server/config/gorm.go
@@ -1,10 +1,16 @@
 package config
 
 type Mysql struct {
+	Dsn          string `mapstructure:"dsn" json:"dsn" yaml:"dsn"`
+	//Deprecated: use Dsn
 	Path         string `mapstructure:"path" json:"path" yaml:"path"`
+	//Deprecated: use Dsn
 	Config       string `mapstructure:"config" json:"config" yaml:"config"`
+	//Deprecated: use Dsn
 	Dbname       string `mapstructure:"db-name" json:"dbname" yaml:"db-name"`
+	//Deprecated: use Dsn
 	Username     string `mapstructure:"username" json:"username" yaml:"username"`
+	//Deprecated: use Dsn
 	Password     string `mapstructure:"password" json:"password" yaml:"password"`
 	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"maxIdleConns" yaml:"max-idle-conns"`
 	MaxOpenConns int    `mapstructure:"max-open-conns" json:"maxOpenConns" yaml:"max-open-conns"`
